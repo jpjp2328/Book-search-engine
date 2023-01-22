b63e0817adb92ffc6c12171ba9f5ae1ab1849c31
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2023-01-22 18:15:32 +1100

    typeDef added query and mutation

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,17 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook()
+    removeBook()
+  }
+  
+`;
+
+module.exports = typeDefs;
